288ef58737332dd113b3ea249e82d6789f77d256
Iuri Dias <iuribatdias@gmail.com>
2021-04-14 16:24:09 +0100

    Add project files.

---
+++ CallReg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace CallReg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Call Reg \n");
+            Console.WriteLine("GiraKuza");
+            ConsoleKeyInfo firstKey = Console.ReadKey();
+            if(firstKey.Key == ConsoleKey.UpArrow)
+            {
+
+            }
+            else
+            {
+                string oi = string.Concat(firstKey.Key.ToString(), Console.ReadLine());
+                Console.WriteLine(oi + " written");
+                Console.ReadLine();
+            }
+        }
+    }
+}
