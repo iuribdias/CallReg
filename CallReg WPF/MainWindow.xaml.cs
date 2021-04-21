8982bc19feff8644c0e33ddf915c12ecfaa955b0
Iuri Dias <iuribatdias@gmail.com>
2021-04-21 15:07:26 +0100

    Started to work on the file writting logic.

--- CallReg WPF/MainWindow.xaml.cs
+++ CallReg WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace CallReg_WPF
 {
@@ -32,7 +33,7 @@ namespace CallReg_WPF
 
         private void Maximize_Click(object sender, RoutedEventArgs e)
         {
-            if(Application.Current.MainWindow.WindowState == WindowState.Normal)
+            if (Application.Current.MainWindow.WindowState == WindowState.Normal)
             {
                 Application.Current.MainWindow.WindowState = WindowState.Maximized;
             }
@@ -45,7 +46,51 @@ namespace CallReg_WPF
 
         private void bCommit_Click(object sender, RoutedEventArgs e)
         {
-            
+            var currentData = new List<Data>
+            {
+                new Data(){
+                    id=int.Parse(idTextbox.Text),
+                    callId=int.Parse(nrTextbox.Text),
+                    name=nameTextbox.Text,
+                    addressVer=(bool)addressCheckbox.IsChecked,
+                    situation=new TextRange(situationTextBox.Document.ContentStart,
+                    situationTextBox.Document.ContentEnd).Text,
+                    olt=oltTextbox.Text,
+                    cell=cellTextbox.Text,
+                    techSee=(bool)techseeCheckbox.IsChecked,
+                    appSmartrouter=(bool)smartrouterCheckbox.IsChecked,
+                    icr=(bool)icrCheckbox.IsChecked
+                }
+            };
+            List<string> textToWrite = new List<string>();
+            foreach(Data a in currentData)
+            {
+                textToWrite.Add("ID: " + a.id.ToString());
+                textToWrite.Add("Nº de od liga: " + a.callId.ToString());
+                textToWrite.Add("Nome: " + a.name);
+                textToWrite.Add("Validação morada: " + a.addressVer.ToString());
+                textToWrite.Add("Situação: " + a.situation);
+                textToWrite.Add("Central/Olt: " + a.olt);
+                textToWrite.Add("Célula: " + a.cell);
+                textToWrite.Add("TechSee: " + a.techSee.ToString());
+                textToWrite.Add("App SmartRouter: " + a.appSmartrouter.ToString());
+                textToWrite.Add("Inquerito ICR: " + a.icr.ToString());
+            }
+            File.WriteAllLines(@"C:\Users\Iuri Dias\Desktop\test.txt", textToWrite);
         }
+    }
+    public class Data
+    {
+        public int id { get; set; }
+        public int callId { get; set; }
+        public string name { get; set; }
+        public bool addressVer { get; set; }
+        public string situation { get; set; }
+        public string olt { get; set; }
+        public string cell { get; set; }
+        public bool techSee { get; set; }
+        public bool appSmartrouter { get; set; }
+        public bool icr { get; set; }
+
     }
 }
